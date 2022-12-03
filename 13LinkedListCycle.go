3830de7ebb344d5629ed3cad4cb62c5c10e3960f
Upasana Ghosh <upasana05ghosh@gmail.com>
2022-12-03 16:36:53 +0530

    Linked List cycle go code added

---
+++ 13LinkedListCycle.go
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/linked-list-cycle/description/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	fast := head
+	slow := head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+
+		if fast == slow {
+			return true
+		}
+	}
+
+	return false
+
+}
