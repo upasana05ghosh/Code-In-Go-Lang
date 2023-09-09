2e167c887f9838707030d5547a2ef70d4703f914
Upasana Ghosh <upasana05ghosh@gmail.com>
2023-09-09 11:39:54 +0530

    Odd Even Linked List

---
+++ 73OddEvenLinkedList.go
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/odd-even-linked-list/description/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	odd := head
+	even, evenHead := head.Next, head.Next
+
+	for even != nil && even.Next != nil {
+		odd.Next = odd.Next.Next
+		even.Next = even.Next.Next
+		odd = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return head
+}
