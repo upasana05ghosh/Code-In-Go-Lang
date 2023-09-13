fe7f13bb10721358634d7f424e13a70bcadd9d0e
Upasana Ghosh <upasana05ghosh@gmail.com>
2023-09-13 19:21:44 +0530

    LCA of Binary Tree

---
+++ 77LCABinaryTree.go
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	}
+	if left != nil {
+		return left
+	}
+	if right != nil {
+		return right
+	}
+	return nil
+}
