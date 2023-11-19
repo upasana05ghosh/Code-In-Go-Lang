7c241159b59d01b258e9607d9059da3c65c83ab3
Upasana Ghosh <upasana05ghosh@gmail.com>
2023-11-19 08:07:41 +0530

    Intersection Of linked list

---
+++ 142IntersectionOfLinkedList.go
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/intersection-of-two-linked-lists/description//**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+    a := headA
+    b := headB
+
+    for a!= b {
+        if a != nil {
+            a = a.Next
+        } else {
+            a = headB
+        }
+
+        if b != nil {
+            b = b.Next
+        } else {
+            b = headA
+        }
+    }
+
+    return a
+}
